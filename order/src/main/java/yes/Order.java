40405551a8a36318dfe157970eb978b42e135700
kmg28801 <kmg08801@gmail.com>
2020-11-03 15:48:04 +0900

    1st commit

---
+++ order/src/main/java/yes/Order.java
@@ -0,0 +1,85 @@
+package yes;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Order_table")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String productId;
+    private Integer qty;
+    private String status;
+
+    @PrePersist
+    public void onPrePersist(){
+        Ordered ordered = new Ordered();
+        BeanUtils.copyProperties(this, ordered);
+        ordered.publishAfterCommit();
+
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        yes.external.Pay pay = new yes.external.Pay();
+        // mappings goes here
+        OrderApplication.applicationContext.getBean(yes.external.PayService.class)
+            .payment(pay);
+
+
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        OrderCancelled orderCancelled = new OrderCancelled();
+        BeanUtils.copyProperties(this, orderCancelled);
+        orderCancelled.publishAfterCommit();
+
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        yes.external.Pay pay = new yes.external.Pay();
+        // mappings goes here
+        OrderApplication.applicationContext.getBean(yes.external.PayService.class)
+            .paymentcancel(pay);
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+
+}
